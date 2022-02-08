fd9f1140bc6868d04e51b3bf7567074673c40d7e
Travis Hegner <travis.hegner@gmail.com>
2022-02-07 19:45:54 -0500

    progress on watcher

--- bird/sibling_watcher.go
+++ bird/sibling_watcher.go
@@ -30,11 +30,11 @@ func NewSiblingWatcher(client Client, interval time.Duration) *SiblingWatcher {
 	}
 }
 
-func (sw *SiblingWatcher) Watch(prefix *net.IPNet, ch chan SiblingUpdate, done chan struct{}) {
+func (sw *SiblingWatcher) Watch(prefix *net.IPNet, ch chan *SiblingUpdate, done chan struct{}) {
 	go sw.watch(prefix, ch, done)
 }
 
-func (sw *SiblingWatcher) watch(prefix *net.IPNet, ch chan SiblingUpdate, done chan struct{}) {
+func (sw *SiblingWatcher) watch(prefix *net.IPNet, ch chan *SiblingUpdate, done chan struct{}) {
 	timer := time.NewTimer(sw.interval)
 
 	for {
@@ -54,7 +54,50 @@ func (sw *SiblingWatcher) watch(prefix *net.IPNet, ch chan SiblingUpdate, done c
 	}
 }
 
-func (sw *SiblingWatcher) reconcile(prefix *net.IPNet) ([]SiblingUpdate, error) {
+func (sw *SiblingWatcher) reconcile(prefix *net.IPNet) ([]*SiblingUpdate, error) {
+	updates := make([]*SiblingUpdate, 0)
+
+	news, err := sw.newSiblings(prefix)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to retrive new sibling set")
+	}
+
+	olds, ok := sw.state[prefix.String()]
+	if !ok {
+		olds = make(map[string]struct{})
+	}
+
+	for sibling := range news {
+		_, ok := olds[sibling]
+		if ok {
+			continue
+		}
+		updates = append(updates, &SiblingUpdate{
+			Prefix:  prefix,
+			Address: net.ParseIP(sibling),
+			Added:   true,
+		})
+	}
+
+	for sibling := range olds {
+		_, ok := news[sibling]
+		if ok {
+			continue
+		}
+		updates = append(updates, &SiblingUpdate{
+			Prefix:  prefix,
+			Address: net.ParseIP(sibling),
+			Added:   false,
+		})
+	}
+
+	sw.state[prefix.String()] = news
+
+	return updates, nil
+}
+
+func (sw *SiblingWatcher) newSiblings(prefix *net.IPNet) (map[string]struct{}, error) {
+	siblings := make(map[string]struct{})
 	lines, err := sw.client.Query(fmt.Sprintf("show route %v where source = RTS_BGP", prefix))
 	if err != nil {
 		return nil, errors.Wrap(err, "error getting sibling routes from bird")
@@ -67,8 +110,8 @@ func (sw *SiblingWatcher) reconcile(prefix *net.IPNet) ([]SiblingUpdate, error) 
 		}
 		split2 := strings.Split(split1[1], "on")
 		address := strings.TrimSpace(split2[0])
-		fmt.Println(address)
+		siblings[address] = struct{}{}
 	}
 
-	return nil, nil
+	return siblings, nil
 }
--- main.go
+++ main.go
@@ -2,39 +2,27 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
-	"github.com/travishegner/fdbsync/bird"
+const (
+	birdSocket = "/var/run/bird/bird.ctl"
 )
 
 func main() {
-	birdc, err := bird.NewSocketClient("/var/run/bird/bird.ctl", 4096)
-	if err != nil {
-		log.Fatalf("failed to create bird client: %v", err)
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+Control:
+	for {
+		select {
+		case s := <-sig:
+			fmt.Printf("received %v signal, quitting\n", s)
+			break Control
+		}
 	}
 
-	lines, err := birdc.Query("show route 10.1.0.0/24")
-	if err != nil {
-		log.Fatalf("failed to query bird client: %v", err)
-	}
-
-	for _, line := range lines {
-		fmt.Println(line)
-	}
-
-	/*
-			sig := make(chan os.Signal, 1)
-			signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-
-		Control:
-			for {
-				select {
-				case s := <-sig:
-					fmt.Printf("received %v signal, quitting\n", s)
-					break Control
-				}
-			}
-
-			close(sig)
-	*/
+	close(sig)
 }

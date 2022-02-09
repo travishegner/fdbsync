9535c188a8cf47f6d06746dfe8f54f3f605ab015
Travis Hegner <travis.hegner@gmail.com>
2022-02-09 15:48:19 -0500

    it works, it really works

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all: build/fdbsync
 rebuild: clean build/fdbsync
 
 clean:
-	rm -rf bin/*
+	rm -rf build/*
 
 build/fdbsync:
 	CGO_ENABLED=0 go build -o build/fdbsync
--- main.go
+++ main.go
@@ -9,74 +9,160 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/gobwas/glob"
 	"github.com/travishegner/fdbsync/bird"
 	"github.com/vishvananda/netlink"
 )
 
 const (
-	birdSocket       = "/var/run/bird/bird.ctl"
-	birdInterval     = 5 * time.Second
-	interfacePattern = "mv_*"
-	containerCIDR    = "10.1.0.0/16"
+	birdSocket    = "/var/run/bird/bird.ctl"
+	birdInterval  = 5 * time.Second
+	containerCIDR = "10.1.0.0/16"
+	linkGlob      = "mv_*"
 )
 
 func main() {
 	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1)
 
 	done := make(chan struct{})
-	linkUpdates := make(chan netlink.LinkUpdate, 1024)
-	siblingUpdates := make(chan *bird.SiblingUpdate, 1024)
+	addrUpdates := make(chan netlink.AddrUpdate, 1024)
+	siblingUpdates := make(chan *SiblingUpdate, 1024)
 
-	err := netlink.LinkSubscribe(linkUpdates, done)
+	err := netlink.AddrSubscribe(addrUpdates, done)
 	if err != nil {
-		log.Fatalf("failed to subscribe to link changes: %v", err)
+		log.Fatalf("failed to subscribe to route changes: %v", err)
 	}
 
-	links, err := netlink.LinkList()
+	watchers := make(map[string]*SiblingWatcher)
+
+	_, cnet, err := net.ParseCIDR(containerCIDR)
 	if err != nil {
-		log.Fatalf("failed to get current links")
+		log.Fatalf("failed to parse container CIDR: %v\n", containerCIDR)
 	}
 
-	globber := glob.MustCompile(interfacePattern)
-	birdClient, err := bird.NewSocketClient(birdSocket)
+	addrs, err := netlink.AddrList(nil, netlink.FAMILY_ALL)
 	if err != nil {
-		log.Fatalf("failed to get bird client: %v\n", err)
+		log.Fatalf("failed to get system link list")
 	}
-	watcher := bird.NewSiblingWatcher(birdClient, birdInterval)
 
-	for _, link := range links {
-		if !globber.Match(link.Attrs().Name) {
+	for _, addr := range addrs {
+		if !cnet.Contains(addr.IP) {
 			continue
 		}
-		addrs, err := netlink.AddrList(link, netlink.FAMILY_ALL)
+
+		_, prefix, _ := net.ParseCIDR(addr.IPNet.String())
+
+		c, err := bird.NewSocketClient(birdSocket)
 		if err != nil {
-			log.Printf("failed to get addresses for link %v: %v", link.Attrs().Name, err)
+			log.Printf("failed to create new socket client for: %v\n", prefix)
+			log.Printf("error: %v\n", err)
 			continue
 		}
-		for _, addr := range addrs {
-			_, net, err := net.ParseCIDR(addr.IPNet.String())
-			if err != nil {
-				log.Printf("failed to get network id for link %v: %v", link.Attrs().Name, err)
-				continue
-			}
-			watcher.Watch(net, siblingUpdates)
-		}
+		w := NewSiblingWatcher(
+			birdInterval,
+			c,
+			prefix,
+			siblingUpdates,
+		)
+
+		w.Watch()
+		watchers[prefix.String()] = w
 	}
 
 Control:
 	for {
 		select {
-		case lu := <-linkUpdates:
-			fmt.Println(lu)
+		case au := <-addrUpdates:
+			if !cnet.Contains(au.LinkAddress.IP) {
+				continue
+			}
+
+			_, prefix, _ := net.ParseCIDR(au.LinkAddress.String())
+
+			if !au.NewAddr {
+				fmt.Printf("addr %v deleted\n", au.LinkAddress)
+
+				//an overlay network was detached, stop and delete it's watcher
+				w, ok := watchers[prefix.String()]
+				if !ok {
+					fmt.Printf("no watcher found for %v\n", prefix)
+					continue
+				}
+				fmt.Printf("stopping watcher for %v\n", prefix)
+				w.Stop()
+				delete(watchers, prefix.String())
+
+				continue
+			}
+
+			_, ok := watchers[prefix.String()]
+			if ok {
+				fmt.Printf("duplicate watcher found for %v refusing to start another one\n", prefix)
+				continue
+			}
+
+			//a new overlay network attached, create and start a new watcher
+			c, err := bird.NewSocketClient(birdSocket)
+			if err != nil {
+				log.Printf("failed to create new socket client for %v\n", prefix)
+				log.Printf("error: %v\n", err)
+				continue
+			}
+			w := NewSiblingWatcher(
+				birdInterval,
+				c,
+				prefix,
+				siblingUpdates,
+			)
+			watchers[prefix.String()] = w
+
+			log.Printf("starting watcher for %v\n", prefix)
+			w.Watch()
 		case su := <-siblingUpdates:
-			fmt.Println(su)
+			if su.Added {
+				//detected a new sibling connected to an overlay
+				log.Printf("adding sibling %v to fdb for %v", su.Sibling, su.Prefix)
+				err := addSibling(su.Prefix, su.Sibling)
+				if err != nil {
+					log.Printf("failed to add sibling %v to forwarding database for %v\n", su.Sibling, su.Prefix)
+					log.Printf("error: %v\n", err)
+				}
+				continue
+			}
+			//detected a sibling disconnecting from an overlay
+			log.Printf("deleting sibling %v from fdb for %v", su.Sibling, su.Prefix)
+			err := delSibling(su.Prefix, su.Sibling)
+			if err != nil {
+				log.Printf("failed to delete sibling %v from forwarding database for %v\n", su.Sibling, su.Prefix)
+				log.Printf("error: %v\n", err)
+			}
 		case s := <-sig:
-			fmt.Printf("received %v signal, quitting\n", s)
-			break Control
+			switch s {
+			case syscall.SIGUSR1:
+				fmt.Println()
+				fmt.Println("Dumping State Tables")
+				fmt.Println("Watchers:")
+				for k := range watchers {
+					fmt.Printf("\t%v\n", k)
+				}
+				fmt.Println()
+			case syscall.SIGINT:
+				fallthrough
+			case syscall.SIGKILL:
+				fmt.Printf("received %v signal, quitting\n", s)
+				break Control
+			default:
+				continue
+			}
 		}
 	}
 
+	for _, watcher := range watchers {
+		watcher.Stop()
+	}
+
+	close(done)
 	close(sig)
+
+	fmt.Println("tetelestai")
 }
